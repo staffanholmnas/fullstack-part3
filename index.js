7ac5b01a289752ec9c29aa8912856d323a1d1982
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-02 10:29:15 +0300

    Do exercise 3.4

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/info', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    console.log("Deleted", person.name)
+    console.log(person.name, "was deleted")
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
