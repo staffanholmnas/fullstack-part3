2a8fc771619eb734f8cb8dad39133f7a0ba4c872
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-04 12:08:18 +0300

    Do exercise 3.11

---
+++ build/precache-manifest.9631406d2d8c20075e7128a7d04468d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a99158140d871e4925e29b9d2132e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e59330ccbc577f265f7",
+    "url": "/static/css/main.8964c925.chunk.css"
+  },
+  {
+    "revision": "c0561943e19394f17379",
+    "url": "/static/js/2.44d5a009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44d5a009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e59330ccbc577f265f7",
+    "url": "/static/js/main.c759edbc.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (request, response) => JSON.stringify(request.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
