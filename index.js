b51a1a305f2c87833e1b049e0351a917fef17a2b
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-13 12:32:13 +0300

    Do ex 3.17

--- index.js
+++ index.js
@@ -68,6 +68,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const generateId = () => {
     const randomId = Math.floor(Math.random() * 100000000000)
     return randomId
