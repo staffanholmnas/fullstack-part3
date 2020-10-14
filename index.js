f88fa99ce731169eb4436984ff57351e76657bc0
staffanholmnas <staffan.holmnas@centria.fi>
2020-10-14 11:08:19 +0300

    Do ex 3.19

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const generateId = () => {
     return randomId
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (body.name === undefined) {
@@ -109,15 +109,19 @@ app.post('/api/persons', (request, response) => {
     })
 
     person.save().then(savedPerson => {
-        response.json(savedPerson)
+        response.json(savedPerson.toJSON())
     })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    }
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
+    
     next(error)
 }
 app.use(errorHandler)
